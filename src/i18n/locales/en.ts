afff910ba28517da07f2f0f16361ab53bc3bc28d
dl <zhongdalin@hclife.cn>
2023-09-12 19:49:26 +0800

    feat: change

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -1,5 +1,5 @@
 export default {
-  "nav.home":"Home",
+  "nav.home": "Home",
   "nav.pricing": "Pricing",
   "nav.about": "About",
   "nav.tutorial": "Tutorial",
@@ -8,79 +8,79 @@ export default {
   "about.title": "About",
 
   // 首页
-  "home.headline1":"Notion AI",
-  "home.headline2":"Notion AI is a plugin developed based on Notion's official AI.<wbr /> By subscribing to this plugin, you can enjoy the same Notion AI experience as the official one for less than 1/4 of the original price. The plugin supports multiple systems, including Mac, Windows, and Android.",
-  "home.menu":"Purchase",
-  "home.menu2":"Add to Chrome",
+  "home.headline1": "Notion AI",
+  "home.headline2": "Notion AI is a plugin developed based on Notion's official AI. By subscribing to this plugin, you can enjoy the same Notion AI experience as the official one for less than 1/4 of the original price. The plugin supports multiple systems, including Mac, Windows, and Android.",
+  "home.menu": "Purchase",
+  "home.menu2": "Add to Chrome",
 
-  "home.mid.headline1":"The best alternative to a Notion AI subscription!",
-  "home.mid.headline2":"Notion AI plugin provides Notion users with an affordable and user-friendly AI alternative.",
+  "home.mid.headline1": "The best alternative to a Notion AI subscription!",
+  "home.mid.headline2": "Notion AI plugin provides Notion users with an affordable and user-friendly AI alternative.",
 
-  "home.features1":"Native Notion AI experience",
-  "home.descriptions1":"This plugin and the Notion AI subscription within the Notion software provide exactly the same experience, with perfect performance.",
-  
-  "home.features2":"Super affordable and Cost-effective",
-  "home.descriptions2":"Super affordable, only 1/4 of the official price.Enable more people to enjoy the convenience brought by Notion AI.",
- 
-  "home.features3":"Supports multiple systems",
-  "home.descriptions3":"Supports Notion clients for Mac and Windows, as well as all Chrome-based browsers and Andriod.",
-  
-  "home.enjoy":"Try Notion AI Now!",
-  "home.enjoy2":"The best alternative to a Notion AI subscription!",
-  "home.button":"Try Now!",
+  "home.features1": "Native Notion AI experience",
+  "home.descriptions1": "This plugin and the Notion AI subscription within the Notion software provide exactly the same experience, with perfect performance.",
+
+  "home.features2": "Super affordable and Cost-effective",
+  "home.descriptions2": "Super affordable, only 1/4 of the official price.Enable more people to enjoy the convenience brought by Notion AI.",
+
+  "home.features3": "Supports multiple systems",
+  "home.descriptions3": "Supports Notion clients for Mac and Windows, as well as all Chrome-based browsers and Andriod.",
+
+  "home.enjoy": "Try Notion AI Now!",
+  "home.enjoy2": "The best alternative to a Notion AI subscription!",
+  "home.button": "Try Now!",
 
 
   // 价格
-  "store.headline":"Pricing",
-  "store.headline1":"Simple & Predictable pricing. No Surprises.",
-  
+  "store.headline": "Pricing",
+  "store.headline1": "Simple & Predictable pricing. No Surprises.",
+
   "store.plan1": "Monthly",
-  "store.price1":"$2.5",
-  "store.month1":"1 month validity period",
-  "store.month2":"Unlimited AI Response",
-  "store.month3":"Support 20+ payment methods",
-  "store.month4":"Alipay and WeChat Pay",
-  "store.button1":"Purchase",
-  
+  "store.price1": "$2.5",
+  "store.month1": "1 month validity period",
+  "store.month2": "Unlimited AI Response",
+  "store.month3": "Support 20+ payment methods",
+  "store.month4": "Alipay and WeChat Pay",
+  "store.button1": "Purchase",
+
   "store.plan2": "Yearly",
-  "store.price2":"$26",
-  "store.year1":"1 year validity period",
-  "store.year2":"Unlimited AI Response",
-  "store.year3":"Support 20+ payment methods",
-  "store.year4":"Alipay and WeChat Pay",
-  "store.button2":"Purchase",
-      
+  "store.price2": "$26",
+  "store.year1": "1 year validity period",
+  "store.year2": "Unlimited AI Response",
+  "store.year3": "Support 20+ payment methods",
+  "store.year4": "Alipay and WeChat Pay",
+  "store.button2": "Purchase",
+
   "store.plan3": "Lifetime",
-  "store.price3":"$39.99",
-  "store.life1":"Lifetime membership",
-  "store.life2":"Unlimited AI Response",
-  "store.life3":"Support 20+ payment methods",
-  "store.life4":"Alipay and WeChat Pay",
-  "store.button3":"Purchase",
-            
+  "store.price3": "$39.99",
+  "store.life1": "Lifetime membership",
+  "store.life2": "Unlimited AI Response",
+  "store.life3": "Support 20+ payment methods",
+  "store.life4": "Alipay and WeChat Pay",
+  "store.button3": "Purchase",
+
 
   // 关于我们
-  "about.title1":"About",
-  "about.title2":"We are a small passionate team.",
-  "about.title3":"Work faster. Write better. Think bigger.",
-  "about.team":"We're a multi-cultural team from around the world! We come from diverse backgrounds, bringing different personalities, experiences and skills to the job. This is what makes our team so special.",
+  "about.title1": "About",
+  "about.title2": "We are a small passionate team.",
+  "about.title3": "Work faster. Write better. Think bigger.",
+  "about.team": "We're a multi-cultural team from around the world! We come from diverse backgrounds, bringing different personalities, experiences and skills to the job. This is what makes our team so special.",
 
   // 使用教程
-  "blog.title":"Blog and Tutorial",
-  "blog.title2":"The Tutorial of Notion AI Plugin and The Using tips of Notion",
-  
-  
-  // 联系我们
-  "team.title":"Contact",
-  "team.title2":"We are here to help.",
-  "team.title3":"Contact LanDrop",
-  "team.title4":"Have something to say? We are here to help. Fill up the form or send email or call phone.",
-  "team.site":"1744 Sanfransico, CA 63093",
-  "team.email":"admin@qunboost.com",
+  "blog.title": "Blog and Tutorial",
+  "blog.title2": "The Tutorial of Notion AI Plugin and The Using tips of Notion",
 
-  "team.fullname":"Full Name",
-  "team.address":"Your Email",
-  "team.content":"Your Message",
-  "team.button":"Send Message",
+
+  // 联系我们
+  "team.title": "Contact",
+  "team.title2": "We are here to help.",
+  "team.title3": "Contact LanDrop",
+  "team.title4": "Have something to say? We are here to help. Fill up the form or send email or call phone.",
+  "team.site": "1744 Sanfransico, CA 63093",
+  "team.email": "admin@qunboost.com",
+
+  "team.fullname": "Full Name",
+  "team.address": "Your Email",
+  "team.content": "Your Message",
+  "team.button": "Send Message",
 
 }
